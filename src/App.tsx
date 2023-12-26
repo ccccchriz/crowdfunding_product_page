a6cf3822afe2f55c3daf3d649486107f550e3e79
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-26 18:45:37 +0100

    added data import and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 
+interface tierTypes {
+  name: string;
+  min_money: number;
+  description: string;
+  left: number;
+  extra_info: string;
+}
+
+interface dataTypes {
+  company: string;
+  short_info: string;
+  money_wanted: number;
+  money_collected: number;
+  backers: number;
+  days_left: number;
+  about: String[];
+  tiers: tierTypes[];
+  thanks_message: string;
+  no_reward_info: string;
+}
+
 function App() {
+  const [data, setData] = useState<dataTypes>();
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((data) => data.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
     <>
       <Header />
+      {data ? <>data</> : <p>Loading</p>}
     </>
   );
 }
