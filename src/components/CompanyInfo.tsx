2bcd48f9a425072645138d7ed58b8bfc0a0d2ef8
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-26 21:06:35 +0100

    finished company info

--- src/components/CompanyInfo.tsx
+++ src/components/CompanyInfo.tsx
@@ -10,10 +10,21 @@ export default function CompanyInfo({
   short_info,
 }: CompanyInfoProps) {
   return (
-    <div className="bg-white p-4 flex flex-col items-center -mt-8 rounded-lg gap-2">
+    <div className="bg-white p-4 flex flex-col items-center -mt-8 rounded-lg gap-4">
       <img src={logo} alt="" className="size-14 -mt-10" />
       <h2 className="font-bold text-center mt-2 text-2xl">{company}</h2>
       <p className="text-center text-neutral-dark-gray">{short_info}</p>
+      <div className="grid grid-cols-[1fr_auto] w-full gap-2">
+        <button
+          type="button"
+          className="text-white bg-primary-cyan font-bold py-2 px-2 rounded-full"
+        >
+          Back this project
+        </button>
+        <button type="button">
+          <img src="assets/images/icon-bookmark.svg" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
