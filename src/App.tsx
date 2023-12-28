5862a02f3cc0c362552bfdb1db5e811697c108fa
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-12-28 21:23:16 +0100

    thanks modal added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "./components/Header";
 import CompanyInfo from "./components/CompanyInfo";
 import ProjectStats from "./components/ProjectStats";
 import AboutProject from "./components/AboutProject";
+import ThanksDialog from "./components/ThanksDialog";
 
 interface tierTypes {
   name: string;
@@ -29,6 +30,8 @@ interface dataTypes {
 function App() {
   const [data, setData] = useState<dataTypes>();
 
+  const thanksDialog = useRef<HTMLDialogElement>(null);
+
   useEffect(() => {
     fetch("data.json")
       .then((data) => data.json())
@@ -39,20 +42,30 @@ function App() {
     <>
       <Header />
       {data ? (
-        <main className="bg-neutral-light-gray p-4 flex flex-col gap-4 font-primary">
-          <CompanyInfo
-            company={data.company}
-            logo={data.logo}
-            short_info={data.short_info}
-          />
-          <ProjectStats
-            money_collected={data.money_collected}
-            money_wanted={data.money_wanted}
-            backers={data.backers}
-            days_left={data.days_left}
-          />
-          <AboutProject about={data.about} tiers={data.tiers} />
-        </main>
+        <>
+          <main className="bg-neutral-light-gray p-4 flex flex-col gap-4 font-primary">
+            <CompanyInfo
+              company={data.company}
+              logo={data.logo}
+              short_info={data.short_info}
+            />
+            <ProjectStats
+              money_collected={data.money_collected}
+              money_wanted={data.money_wanted}
+              backers={data.backers}
+              days_left={data.days_left}
+            />
+            <AboutProject about={data.about} tiers={data.tiers} />
+            <button
+              type="button"
+              className="bg-black p-16 text-white"
+              onClick={() => thanksDialog.current!.showModal()}
+            >
+              OPEN DIALOG
+            </button>
+          </main>
+          <ThanksDialog message={data.thanks_message} ref={thanksDialog} />
+        </>
       ) : (
         <p>Loading</p>
       )}
---
+++ src/components/ThanksDialog.tsx
@@ -0,0 +1,31 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+interface ThanksDialogProps {
+  message: string;
+}
+
+const ThanksDialog = forwardRef(
+  ({ message }: ThanksDialogProps, ref: React.Ref<HTMLDialogElement>) => {
+    const dialog = useRef<HTMLDialogElement>(null);
+    useImperativeHandle(ref, () => dialog.current!, []);
+
+    return (
+      <dialog ref={dialog}>
+        <div className="grid place-items-center text-center gap-4 px-4 py-12 font-primary rounded-lg">
+          <img src="assets/images/icon-check.svg" alt="" />
+          <h2 className="text-xl font-bold">Thanks for your support</h2>
+          <p className="text-neutral-dark-gray">{message}</p>
+          <button
+            type="button"
+            onClick={() => dialog.current!.close()}
+            className="bg-primary-cyan text-white px-8 py-2 rounded-full font-medium"
+          >
+            Got it!
+          </button>
+        </div>
+      </dialog>
+    );
+  }
+);
+
+export default ThanksDialog;
