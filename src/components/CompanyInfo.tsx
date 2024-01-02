76ae94860ea9f1ab62a4757d89f705c37adea28d
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-02 16:25:25 +0100

    desktop design

--- src/components/CompanyInfo.tsx
+++ src/components/CompanyInfo.tsx
@@ -16,16 +16,22 @@ export default function CompanyInfo({
       <img src={logo} alt="" className="size-14 -mt-10" />
       <h2 className="font-bold text-center mt-2 text-2xl">{company}</h2>
       <p className="text-center text-neutral-dark-gray">{short_info}</p>
-      <div className="grid grid-cols-[1fr_auto] w-full gap-2">
+      <div className="w-full gap-2 flex md:justify-between">
         <button
           type="button"
           onClick={() => setModalIndex(-1)}
-          className="text-white bg-primary-cyan font-bold py-2 px-2 rounded-full"
+          className="text-white bg-primary-cyan font-bold py-2 px-8 rounded-full grow md:grow-0"
         >
           Back this project
         </button>
-        <button type="button">
+        <button
+          type="button"
+          className="flex items-center gap-4 bg-neutral-light-gray rounded-full md:pr-4"
+        >
           <img src="assets/images/icon-bookmark.svg" alt="" />
+          <span className="sr-only md:not-sr-only mr-4 font-medium">
+            Bookmark
+          </span>
         </button>
       </div>
     </div>
